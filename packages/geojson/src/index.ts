4cdf2fec664dfc975ea2408bbe656f8a23c46271
jessekrubin <jessekrubin@gmail.com>
2024-11-21 11:49:23 -0800

    geojson package

---
+++ packages/geojson/src/index.ts
@@ -0,0 +1,2 @@
+export * from "@jsse/geotypes/geojson";
+export const pkgid = Symbol.for("@jsse/geojson");
---
+++ packages/geotypes/src/geojson.ts
@@ -0,0 +1,3 @@
+export * from "./types/geojson.js"
+export * from "./types/geojson-crs.js"
+export * from "./types/geojson-tippecanoe.js"
