7a22f76fc4fe62a9198b8bf5e89228eba296f947
jessekrubin <jessekrubin@gmail.com>
2025-04-22 16:10:13 -0700

    generate update

--- packages/geojson/src/_dev/geojson-tag.ts
+++ packages/geojson/src/_dev/geojson-tag.ts
@@ -1,20 +1,20 @@
-export enum GeojsonTag {
+export const GeojsonTag = {
   // geojson
-  Geojson = 0b1,
-  Coord2d = 0b10,
-  Coord3d = 0b100,
+  Geojson: 0b1,
+  Coord2d: 0b10,
+  Coord3d: 0b100,
   // geometries
-  Point = 0b1000,
-  MultiPoint = 0b1_0000,
-  LineString = 0b10_0000,
-  MultiLineString = 0b100_0000,
-  Polygon = 0b1000_0000,
-  MultiPolygon = 0b1_0000_0000,
-  GeometryCollection = 0b10_0000_0000,
+  Point: 0b1000,
+  MultiPoint: 0b1_0000,
+  LineString: 0b10_0000,
+  MultiLineString: 0b100_0000,
+  Polygon: 0b1000_0000,
+  MultiPolygon: 0b1_0000_0000,
+  GeometryCollection: 0b10_0000_0000,
   // features
-  Feature = 0b100_0000_0000,
-  FeatureCollection = 0b1000_0000_0000,
-}
+  Feature: 0b100_0000_0000,
+  FeatureCollection: 0b1000_0000_0000,
+} as const;
 
 export const GeojsonTagSymbol = Symbol.for("@jsse/geo/json/tag");
 //
